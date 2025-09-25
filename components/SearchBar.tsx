4625cdc270d992f163bbff3c649e043b46f132b8
bayrem <bayremakka2003@gmail.com>
2025-09-25 21:40:53 +0100

    Develop Search Bar Screen

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,11 +5,13 @@ import {icons} from "@/constants/icons";
 interface Props {
     placeholder: string;
     onPress?: () => void;
+    value: string;
+    onChangeText: (text:string) => void;
 
 }
 
 
-const SearchBar = ({onPress , placeholder}: Props) => {
+const SearchBar = ({onPress , onChangeText, value, placeholder}: Props) => {
     return (
         <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4 ">
             <Image source={icons.search} className="size-5"
@@ -17,9 +19,8 @@ const SearchBar = ({onPress , placeholder}: Props) => {
             <TextInput
                 onPress={onPress}
                 placeholder={placeholder}
-                value=""
-                onChangeText={() => {
-                }}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor={"#a8b5db"}
                 className="flex-1 ml-2 text-white"
             />
